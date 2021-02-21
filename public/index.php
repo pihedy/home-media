2e7469db5d7ae4ca341277b34a6d661a54653245
pihedy <pihedy@gmail.com>
2021-02-21 09:25:57 +0100

    Set simple public file.

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+/** 
+ * @package     Simple home media center
+ * @author      Pihe Edmond
+ * @email       pihedy@gmail.com
+ */
+
+/** 
+ * @var string App Public Directory.
+ */
+define('APP_PUBLIC_DIR', __DIR__);
+
+/** 
+ * @var string App Root Directory.
+ */
+define('APP_ROOT_DIR', APP_PUBLIC_DIR . DIRECTORY_SEPARATOR . '..');
+
+/** 
+ * @var string App Src Directory.
+ */
+define('APP_SRC_DIR', APP_ROOT_DIR . DIRECTORY_SEPARATOR . 'src');
+
+/** 
+ * @var string Call the bootstrap file.
+ */
+require_once APP_SRC_DIR . DIRECTORY_SEPARATOR . 'bootstrap.php';
